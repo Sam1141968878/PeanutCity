055e0c2c797111237b5bda04c96da774db770eba
LiGuanHan <1141968878@qq.com>
2018-03-19 18:44:46 +0800

    '第三次提交,进一步完善登陆功能'

--- Components/Page/LandingPage/RegisteredPage.js
+++ Components/Page/LandingPage/RegisteredPage.js
@@ -21,12 +21,41 @@ import {
   InteractionManager,
   TouchableOpacity,
   Image,
+  Clipboard,
 } from 'react-native';
 import { Container, Content,Form,Label, Item, Input, Icon } from 'native-base';
 
 
 import PublicGoBack from '../../PublicComponent/PublicGoBack'
+import Checkbox from 'teaset/components/Checkbox/Checkbox';
 export default class RegisteredPage extends PureComponent{
+    state={
+        secureTextEntry:false,
+        textFromClipboard:'',
+        checked:false,
+    }
+    ChangeSecureTextEntry=()=>{
+        this.setState({
+            secureTextEntry:!this.state.secureTextEntry,
+        })
+    }
+
+    //从剪贴板中读取字符串
+    pasteFromClipboard() {
+      Clipboard.getString().then(
+        (textFromClipboard) => {
+          this.setState({
+            textFromClipboard:textFromClipboard
+          })
+        }
+      ).catch(
+        (error) => {
+          console.log("从剪贴板中读取数据错误!");
+          console.log(error);
+        }
+      );
+    }
+
   render() {
     const {state,goBack}=this.props.navigation;
     return (
@@ -46,8 +75,20 @@ export default class RegisteredPage extends PureComponent{
                 <Item floatingLabel>
                     <Label><Text style={styles.LabelText}>请输入邀请码</Text> </Label>
                     <Icon active name='md-person-add' style={styles.Icon2}/>
-                    <Input autoCapitalize='none'/>
-                    </Item>
+                    <Input
+                        autoCapitalize='none'
+                        value={this.state.textFromClipboard}
+                        onChange={(text)=>this.setState({
+                            textFromClipboard:text
+                        })}
+                        maxLength={6}
+                    />
+                </Item>
+                    <TouchableOpacity style={styles.redView1}
+                          onPress={this.pasteFromClipboard.bind(this)}
+                    >
+                        <Text style={styles.redText}>粘贴邀请码</Text>
+                    </TouchableOpacity>
                 <Item floatingLabel>
                     <Label><Text style={styles.LabelText}>请输入验证码</Text> </Label>
                     <Icon active name='ios-unlock' />
@@ -56,16 +97,45 @@ export default class RegisteredPage extends PureComponent{
                         autoCapitalize='none'
                     />
                 </Item>
+                <View style={styles.redView2}>
+                    <Text style={styles.redText}>获取验证码</Text>
+                </View>
                 <Item floatingLabel>
                     <Icon active name='key'/>
                     <Label><Text style={styles.LabelText}>请输入6~32位密码</Text> </Label>
                     <Input
                         maxLength={32}
-                        secureTextEntry={true}
+                        secureTextEntry={this.state.secureTextEntry}
                         autoCapitalize='none'
                     />
                 </Item>
             </Form>
+            <TouchableOpacity onPress={this.ChangeSecureTextEntry}
+                              style={styles.Image6View}
+            >
+                {
+                  this.state.secureTextEntry
+                  ?
+                      <Image
+                          source={require('../../../Icons/Down.png')}
+                          style={styles.Image6}
+                      />
+                  :
+                      <Image
+                          source={require('../../../Icons/See.png')}
+                          style={styles.Image6}
+                      />
+                }
+            </TouchableOpacity>
+            <Checkbox
+                title='Large'
+                size='lg'
+                checked={this.state.checked}
+                onChange={checked => this.setState({checked})}
+            />
+            <Text style={styles.Text1}>*同意
+                <Text style={styles.Text2}> &lt;&lt;花生日记App用户协议&gt;&gt; </Text>
+            </Text>
         </Content>
       </Container>
     );
@@ -89,5 +159,52 @@ const styles = StyleSheet.create({
     LabelText:{
         fontSize:14,
         color:'gray',
+    },
+    Image6View:{
+        position:'absolute',
+        top:235,
+        right:10,
+    },
+    Image6:{
+        width:25,
+        height:25,
+    },
+    redView1:{
+        width:100,
+        height:25,
+        borderWidth:1,
+        borderColor:'red',
+        borderRadius:13,
+        justifyContent:'center',
+        alignItems:'center',
+        position:'absolute',
+        right:10,
+        top:100,
+    },
+    redView2:{
+        width:100,
+        height:25,
+        borderWidth:1,
+        borderColor:'red',
+        borderRadius:13,
+        justifyContent:'center',
+        alignItems:'center',
+        position:'absolute',
+        right:10,
+        top:165,
+    },
+    redText:{
+        color:'red'
+    },
+    TextView:{
+        height:30,
+        width:200,
+        backgroundColor:'#000'
+    },
+    Text1:{
+        color:'#000'
+    },
+    Text2:{
+        color:'red'
     }
 });
